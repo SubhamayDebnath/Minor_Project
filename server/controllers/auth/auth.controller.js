2f4f63870683ac6bf3cba101769eda1aaa3f6ed6
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-14 18:44:31 +0530

    "Updated auth controller to include latitude and longitude in user registration, added checks for duplicate email and phone, and modified password hashing and user creation logic."

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -1,32 +1,52 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+import fs from "fs/promises";
+import crypto from "crypto";
+config();
+import User from "../../models/user.model.js";
+
+
 /*
     Register
-*/ 
+*/
 const register = async (req, res, next) => {
   try {
-    const { username,phone, email, password } = req.body;
-    if (!username || !phone || !email || !password) {
+    const { username, phone, email, password, latitude, longitude } = req.body;
+    if (!username || !phone || !email || !password || !latitude || !longitude) {
       req.flash("error_msg", "Please fill in all fields");
       return res.redirect("/register");
     }
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
+    const existingUser = await User.findOne({ $or: [{ email }, { phone }] });
+    if (existingUser.email === email && existingUser.phone === phone) {
+      req.flash("error_msg", "Email and Phone already in use");
+      return res.redirect("/register");
+    }
+    if (existingUser.email === email) {
       req.flash("error_msg", "Email already in use");
       return res.redirect("/register");
     }
-    const hashPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
+    if (existingUser.phone === phone) {
+      req.flash("error_msg", "Phone already in use");
+      return res.redirect("/register");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
       username,
-      phone,
       email,
-      password: hashPassword,
+      phone,
+      password: hashedPassword,
+      location: {
+        type: "Point",
+        coordinates: [longitude, latitude],
+      },
     });
-    if (!user) {
+    if (!newUser) {
       req.flash("error_msg", "Failed to create user");
       return res.redirect("/register");
     }
-    await user.save();
-    res.redirect("/activation");
+    await newUser.save();
+    res.redirect("/login");
   } catch (error) {
     console.log(`Login error : ${error}`);
     res.redirect("/error");
