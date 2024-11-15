37167c5e0fb9b497ee6a278aa3feb52e0e76b163
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-15 13:21:06 +0530

    "Added input validation and error handling to addSkill function in admin controller"

--- server/controllers/admin/admin.controller.js
+++ server/controllers/admin/admin.controller.js
@@ -1,6 +1,25 @@
+import Skill from '../../models/skill.model.js'
 const addSkill=async (req,res) => {
     try {
-        console.log(req.body);
+        const {name} =req.body;
+        if(!name){
+            req.flash("error_msg", "Please enter a name");
+            return res.redirect("/dashboard/skills");
+        }
+        const skill = await Skill.find({name:name});
+        if(skill){
+            req.flash("error_msg", "Skill name is already exist");
+            return res.redirect("/dashboard/skills");
+        }
+        const newSkill=await Skill.create({name});
+        if(!newSkill){
+            req.flash("error_msg", "Failed to add skill");
+            return res.redirect("/dashboard/skills");
+        }
+        await newSkill.save();
+        req.flash("success_msg", "Skill added successfully");
+        return res.redirect("/dashboard/skills");
+
     } catch (error) {
         console.log(`Skill add error : ${error}`);
         res.redirect("/error");
