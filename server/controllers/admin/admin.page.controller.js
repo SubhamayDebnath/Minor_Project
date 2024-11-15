4ae78c155611380bec7e82eed01523ddbc256311
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-15 11:17:05 +0530

    "Updated admin CSS, added users page controller and route, modified dashboard controller and admin layout EJS files"

--- server/controllers/admin/admin.page.controller.js
+++ server/controllers/admin/admin.page.controller.js
@@ -1,4 +1,7 @@
 const adminLayout = "../views/layouts/admin";
+/*
+    Dashboard page
+*/ 
 const dashboard=async (req,res) => {
     try {
         const locals = {
@@ -7,10 +10,26 @@ const dashboard=async (req,res) => {
         };
         res.render("admin/index",{locals,layout:adminLayout,user:req.user})
     } catch (error) {
-        console.log(`Dashboard error : ${error}`);
+        console.log(`Dashboard Page error : ${error}`);
+        res.redirect("/error");
+    }
+}
+/*
+    Users page
+*/ 
+const users =async (req,res) => {
+    try {
+        const locals = {
+            title: "Dashboard - Users",
+            description: "Welcome to Dashboard users",
+        };
+        res.render("admin/users",{locals,layout:adminLayout,user:req.user})
+    } catch (error) {
+        console.log(`Users Page error : ${error}`);
         res.redirect("/error");
     }
 }
 export{
-    dashboard
+    dashboard,
+    users
 }
--- server/routes/admin/admin.routes.js
+++ server/routes/admin/admin.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { dashboard } from "../../controllers/admin/admin.page.controller.js";
+import { dashboard ,users} from "../../controllers/admin/admin.page.controller.js";
 import {isAuthorized} from "../../middlewares/auth.middleware.js"
 const router = Router();
 
 router.get("/",isAuthorized,dashboard)
+router.get("/users",isAuthorized,users);
 
 export default router;
