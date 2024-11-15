33d78df74cae976251d54c6429a4053eb08afdef
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-15 22:41:48 +0530

    "Updated user controller to handle additional user details and image upload, and modified profile view to display error and success messages."

--- server/controllers/user/user.controllers.js
+++ server/controllers/user/user.controllers.js
@@ -1,6 +1,59 @@
+// bloodGroup: 'A+',
+//   allergies: 'dsad',
+//   ' medicalProblems': 'sadsad',
+//   skills: '673738dfda3029df13c978fd',
+//   isAvailable: 'true',
+//   isRescuer: 'true'
+
+import { config } from "dotenv";
+import fs from "fs/promises";
+config();
+import User from "../../models/user.model.js";
+import cloudinary from "../utils/cloudinary.js";
+import sendEmail from "../utils/sendMail.js";
+
 const updateOtherDetails=async (req,res) => {
     try {
-        console.log(req.body);
+        const {bloodGroup,allergies,medicalProblems,skills,isAvailable,isRescuer}=req.body;
+        if(!allergies || medicalProblems){
+            req.flash("error_msg", "Please fill in all fields");
+            return res.redirect("/profile");
+        }
+        let image = "";
+        let public_id = "";
+        if (!req.file) {
+            req.flash("error_msg", "Please upload an image.");
+            return res.redirect("/profile");
+        }
+        if (req.file) {
+            const transformationOptions = {
+              transformation: [
+                {
+                  quality: "auto:low",
+                  fetch_format: "avif",
+                },
+              ],
+            };
+      
+            const cloudinaryResult = await cloudinary.uploader.upload(
+              req.file.path,
+              transformationOptions
+            );
+            image = cloudinaryResult.secure_url;
+            public_id = cloudinaryResult.public_id;
+            fs.rm(req.file.path);
+          }
+        const user = await User.findByIdAndUpdate(req.user._id, {
+            bloodGroup:bloodGroup,
+            allergies:allergies.split(","),
+            medicalProblems:medicalProblems.split(","),
+            skills:skills,
+            isAvailable:isAvailable===true,
+            isRescuer:isRescuer===true
+        })
+        if(!user){
+
+        }
     } catch (error) {
         console.log(`Other details page error : ${error}`);
         res.redirect("/error");
