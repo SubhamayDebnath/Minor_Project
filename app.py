6face79c4744f1f352d4ada06e07f54dc220c6ca
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-12-15 23:19:36 +0530

    "Added mail configuration to app.py"

--- app.py
+++ app.py
@@ -21,7 +21,14 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
 # Mail configuration
-
+# Mail configuration
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = ""
+app.config["MAIL_PASSWORD"] = ""
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 
 # Initialize
 db = SQLAlchemy(app)
